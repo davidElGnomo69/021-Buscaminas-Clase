57a3fde23f87317928af682232da5384f8e45bfa
ionic <ionicharnina@gmail.com>
2019-04-01 19:38:19 +0200

    El listener vacio

--- src/controlador/MyMouseListener.java
+++ src/controlador/MyMouseListener.java
@@ -1,64 +1,13 @@
 package controlador;
 
-import java.awt.Image;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-
-import Modelo.Coordenada;
-import Utiles.Sonido;
-import Utiles.Utiles;
-
-public class MyMouseListener implements MouseListener {
-	
-	
+//Aqui convendría explicar lo del Adapter
+public class MyMouseListener extends MouseAdapter{
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (e.getButton()==1) {}
-
-	}
-		
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-	}// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
